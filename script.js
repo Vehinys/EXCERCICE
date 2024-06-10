e9c6d524912efa2378149a3da11a253826f94e41
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-06-10 15:46:03 +0200

    MAJ BURGER

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ let mybutton = document.getElementById("scrollToTopBtn");
             document.body.scrollTop = 0;
             document.documentElement.scrollTop = 0;
         };
+
+function ShowResponsiveMenu() {
+    var menu = document.getElementById("topnav_responsive_menu");
+    var icon = document.getElementById("top_hamburger_icon");
+    var root = document.getElementById("root");
+
+    if (menu.className === "") {
+        menu.className = "open";
+        icon.className = "open";
+        root.style.overflowY = "hidden";
+    } else {
+        menu.className = "";
+        icon.className = "";
+        root.style.overflowY = "";
+    }
+}        
